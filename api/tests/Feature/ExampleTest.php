abd2793fef6cb0f341ebd3bb090a287ee56f4fa1
Shashank Shekhar <ershashankshekhar00@gmail.com>
2025-01-19 12:25:08 +0530

    HTTP Tests - Update ExampleTest.php

--- api/tests/Feature/ExampleTest.php
+++ api/tests/Feature/ExampleTest.php
@@ -1,7 +1,42 @@
 <?php
 
-it('returns a successful response', function () {
-    $response = $this->get('/');
+namespace Tests\Feature;
 
-    $response->assertStatus(200);
-});
+use Tests\TestCase;
+
+class ExampleTest extends TestCase
+{
+    /**
+     * A basic functional test for registeration.
+     */
+    public function test_making_an_api_registration(): void
+    {
+        $userData = [
+            'name' => 'tester',
+            'email' => 'tester@gmail.com',
+            'password' => '123',
+            'c_password' => '123',
+        ];
+
+        $response = $this->postJson('/api/register', $userData);
+
+        $response
+            ->assertStatus(200);
+    }
+
+    /**
+     * A basic functional test for login.
+     */
+    // public function test_making_an_api_login(): void
+    // {
+    //     $userData = [
+    //         'email' => 'tester@gmail.com',
+    //         'password' => '123',
+    //     ];
+
+    //     $response = $this->postJson('/api/login', $userData);
+
+    //     $response
+    //         ->assertStatus(200);
+    // }
+}
